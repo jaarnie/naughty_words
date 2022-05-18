40110ace9ff689914d275a7b3fc725eb9b15c44c
John Arnold <johnadamarnold@gmail.com>
2022-05-18 19:52:08 +0100

    initial commit

---
+++ lib/naughty_words.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "naughty_words/version"
+
+module NaughtyWords
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/naughty_words/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NaughtyWords
+  VERSION = "0.1.0"
+end
