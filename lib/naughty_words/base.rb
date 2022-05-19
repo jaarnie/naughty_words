6837273f56877fcefe3de9b87d5c41e29823290f
John Arnold <johnadamarnold@gmail.com>
2022-05-19 13:49:28 +0100

    temp - fix config path

--- lib/naughty_words/base.rb
+++ lib/naughty_words/base.rb
@@ -15,7 +15,7 @@ module NaughtyWords
         # TODO: Fix filtering full words ending with 'ing' and repeated words such as 'fuckfuckfuck'
         blacklist_file.each do |line|
           word = line.chomp
-          string.sub!(word, replacement * word.length)
+          string.gsub!(word, replacement * word.length)
         end
 
         string
@@ -24,9 +24,10 @@ module NaughtyWords
       private
 
       def blacklist_file
-        File.open("./config/en.txt") do |file|
-          @blacklist_file ||= file.readlines
-        end
+        txt_file = File.join(File.dirname(File.expand_path(__FILE__)), "config/en.txt")
+        file = File.open(txt_file)
+
+        @blacklist_file ||= file.readlines
       end
     end
   end
