b5a2c5cc6737bdd5413c340e533850e5a95682a2
John Arnold <41114838+jaarnie@users.noreply.github.com>
2022-08-03 07:26:42 +0100

    Add allow/deny list (#1)

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,3 +1,5 @@
+require "tempfile"
+
 RSpec.describe NaughtyWords::Base do
   describe "#profanity?" do
     context "when the string contains profanity" do
@@ -15,6 +17,12 @@ RSpec.describe NaughtyWords::Base do
         expect(NaughtyWords::Base.profanity?(string: string)).to eq(false)
       end
     end
+
+    context "when the string is in both deny and allow lists" do
+      it "returns false" do
+        # Need to create a tempfile to test this
+      end
+    end
   end
 
   describe "#filter" do
@@ -26,7 +34,7 @@ RSpec.describe NaughtyWords::Base do
         expect(NaughtyWords::Base.filter(string: string, replacement: replacement)).to eq("**** you")
       end
 
-      it "returns a string with profanity replaced with the replacement converted into a string" do
+      it "returns a string with profanity replaced with the replacement converted fron an integer into a string" do
         replacement = 5
         string = "fuck you"
 
@@ -39,6 +47,20 @@ RSpec.describe NaughtyWords::Base do
         string = "hello world"
 
         expect(NaughtyWords::Base.filter(string: string, replacement: "*")).to eq(string)
+      end
+    end
+  end
+
+  describe "#show_ilst" do
+    context "when allow list" do
+      it "shows the list" do
+        expect(NaughtyWords.show_list(list: "allow")).to include("scunthorpe")
+      end
+    end
+
+    context "when deny list" do
+      it "shows the list" do
+        expect(NaughtyWords.show_list(list: "deny")).to include("fuck")
       end
     end
   end
